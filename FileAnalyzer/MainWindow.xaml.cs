22e7a7b5f757de289b5e265efa4dbe7489a3b0c4
Александр Фирсов <firsovalex@yandex.ru>
2021-11-17 19:51:36 +0300

    Add checkBox & Copy files

--- FileAnalyzer/MainWindow.xaml.cs
+++ FileAnalyzer/MainWindow.xaml.cs
@@ -22,11 +22,15 @@ namespace FileAnalyzer
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string[] arrayDelFile;
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        public string[] arrayDelCopyFile;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            textBoxPathCopy.Visibility = Visibility.Hidden;
+            buttonPathCopy.Visibility = Visibility.Hidden;
         }
 
 
@@ -43,7 +47,7 @@ namespace FileAnalyzer
             else
             {
                 string[] arrayFiles = Directory.GetFiles(textBoxPath.Text);
-                arrayDelFile = new string[arrayFiles.Length];
+                arrayDelCopyFile = new string[arrayFiles.Length];
 
                 for (int i = 0; i < arrayFiles.Length; i++)
                 {
@@ -57,54 +61,62 @@ namespace FileAnalyzer
                             if (EqualsBytes(fileFirst, fileSecond))
                             {
                                 counter++;
-                                arrayDelFile[i] = arrayFiles[j];
-                                last = j;
+                                if ((bool)checkBoxCopy.IsChecked)
+                                {
+                                    arrayDelCopyFile[i] = arrayFiles[j];
+                                }
+                                else
+                                {
+                                    arrayDelCopyFile[i] = arrayFiles[j];
+                                    last = j;
+                                }
                             }
                         }
                     }
                 }
             }
-            labelCopiesFound.Content = counter;
+            if (counter == 0)
+            {
+                System.Windows.MessageBox.Show("Копии не найдены!", "", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                labelCopiesFound.Content = counter;
+                if ((bool)checkBoxCopy.IsChecked)
+                {
+                    for (int i = 0; i < arrayDelCopyFile.Length; i++)
+                    {
+                        if (arrayDelCopyFile[i] != null)
+                        {
+                            FileInfo fileCopy = new FileInfo(arrayDelCopyFile[i]);
+                            fileCopy.MoveTo(textBoxPathCopy.Text + "\\" + fileCopy.Name);
+                        }
+                    }
+                }
+            }
         }
 
         private void ButtonDel_Click(object sender, RoutedEventArgs e)
         {
             int couter = 0;
 
-            if (arrayDelFile is null)
+            if (arrayDelCopyFile is null)
             {
                 System.Windows.MessageBox.Show("Нет копий, не было проведено сканирование или копии не найдены!", "Нет копий!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else
             {
 
-                for (int i = 0; i < arrayDelFile.Length; i++)
+                for (int i = 0; i < arrayDelCopyFile.Length; i++)
                 {
-                    if (arrayDelFile[i] != null)
+                    if (arrayDelCopyFile[i] != null)
                     {
                         couter++;
-                        File.Delete(arrayDelFile[i]);
+                        File.Delete(arrayDelCopyFile[i]);
                     }
                 }
                 labelCopiesRemoved.Content = couter;
             }
-        }
-
-        private void ButtonPath_Click(object sender, RoutedEventArgs e)
-        {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-
-            folderBrowserDialog.ShowDialog();
-
-            textBoxPath.Text = folderBrowserDialog.SelectedPath;
-        }
-
-        private void ButtonCansel_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxPath.Clear();
-
-            labelCopiesFound.Content = "0";
-            labelCopiesRemoved.Content = "0";
         }
 
         private bool EqualsBytes(FileInfo fileFirst, FileInfo fileSecond)
@@ -132,6 +144,42 @@ namespace FileAnalyzer
             {
                 return false;
             }
+        }
+
+        private void ButtonPathCopy_Click(object sender, RoutedEventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+
+            textBoxPathCopy.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void CheckBoxCopy_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxPathCopy.Visibility = Visibility.Visible;
+            buttonPathCopy.Visibility = Visibility.Visible;
+            buttonDel.Visibility = Visibility.Hidden;
+        }
+
+        private void CheckBoxCopy_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBoxPathCopy.Visibility = Visibility.Hidden;
+            buttonPathCopy.Visibility = Visibility.Hidden;
+            buttonDel.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonPath_Click(object sender, RoutedEventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+
+            textBoxPath.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void ButtonCansel_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxPath.Clear();
+
+            labelCopiesFound.Content = "0";
+            labelCopiesRemoved.Content = "0";
         }
     }
 }
